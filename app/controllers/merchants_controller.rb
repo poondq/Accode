09b2491a6e14b65b72b873c0e2294af95d4e1c08
“poondq” <poondq1@gmail.com>
2016-12-08 19:37:26 +0800

    add calculation

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,7 +1,22 @@
 class MerchantsController < ApplicationController
 	def index
-		  	render json: User.all.to_json
-		  	# render json: User.all.to_json
-	
-  end
+		# params[:lat]
+		 # params[:request_zone]
+		# params[:request_amount]
+
+		if params[:request_amount].present?
+			 @user = User.where("merchant_amount > ?", params[:request_amount])
+			 @user = User.select("merchant_amount > ?", params[:request_amount])
+
+		else
+			@user = User.all
+		end
+
+
+		  	render json: @user.to_json
+		end
+
+	 def show
+	 	render json: User.find(params[:id])
+	 end
 end
